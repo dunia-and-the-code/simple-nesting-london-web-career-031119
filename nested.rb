25345f08c9e89340f4067cd1229e362593ed9f19
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-03-12 16:40:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -107,9 +107,9 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     changing_alan programmer_hash[:alan_kay][:known_for] = "GUI"
+     programmer_hash[:alan_kay][:known_for] = "GUI"
  
-     puts changing_alan
+     puts programmer_hash
 end
 
 
